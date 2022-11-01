7c058aa947a5a30579c05c9431a5c8cbef52263f
justi <justi@DESKTOP-4JKBT94>
2022-10-31 21:14:43 -0500

    Add project files.

---
+++ 3DPrinterController/UserControls/MainControl.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace _3DPrinterController.UserControls
+{
+    partial class MainControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.listBoxPorts = new System.Windows.Forms.ListBox();
+            this.btnOpenPort = new System.Windows.Forms.Button();
+            this.btnMove = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.btnMove, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.listBoxPorts, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnOpenPort, 0, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(686, 393);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // listBoxPorts
+            // 
+            this.listBoxPorts.FormattingEnabled = true;
+            this.listBoxPorts.Location = new System.Drawing.Point(3, 3);
+            this.listBoxPorts.Name = "listBoxPorts";
+            this.listBoxPorts.Size = new System.Drawing.Size(120, 95);
+            this.listBoxPorts.TabIndex = 1;
+            // 
+            // btnOpenPort
+            // 
+            this.btnOpenPort.Location = new System.Drawing.Point(3, 199);
+            this.btnOpenPort.Name = "btnOpenPort";
+            this.btnOpenPort.Size = new System.Drawing.Size(96, 45);
+            this.btnOpenPort.TabIndex = 2;
+            this.btnOpenPort.Text = "Open Port";
+            this.btnOpenPort.UseVisualStyleBackColor = true;
+            this.btnOpenPort.Click += new System.EventHandler(this.btnOpenPort_Click);
+            // 
+            // btnMove
+            // 
+            this.btnMove.Location = new System.Drawing.Point(346, 3);
+            this.btnMove.Name = "btnMove";
+            this.btnMove.Size = new System.Drawing.Size(75, 23);
+            this.btnMove.TabIndex = 1;
+            this.btnMove.Text = "Move";
+            this.btnMove.UseVisualStyleBackColor = true;
+            this.btnMove.Click += new System.EventHandler(this.btnMove_Click);
+            // 
+            // MainControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "MainControl";
+            this.Size = new System.Drawing.Size(842, 496);
+            this.Load += new System.EventHandler(this.MainControl_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ListBox listBoxPorts;
+        private System.Windows.Forms.Button btnOpenPort;
+        private System.Windows.Forms.Button btnMove;
+    }
+}
---
+++ 3DPrinterController/UserControls/MainControl.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3DPrinterController.UserControls
+{
+    public partial class MainControl : UserControl
+    {
+        SerialPort serialPort = new SerialPort();
+        public MainControl()
+        {
+            InitializeComponent();
+            
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+        }
+
+        private void MainControl_Load(object sender, EventArgs e)
+        {
+            // Add all coms to listbox
+            string[] ports = System.IO.Ports.SerialPort.GetPortNames();
+            listBoxPorts.Items.AddRange(ports);
+
+
+        }
+
+        private void btnOpenPort_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort = new SerialPort(listBoxPorts.SelectedItem.ToString(), 115200, Parity.None, 8, StopBits.One);
+                //serialPort.PortName = listBoxPorts.Text;
+                serialPort.Open();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            // Open serialport
+
+            //serialPort.Open();
+
+
+        }
+
+        private void btnMove_Click(object sender, EventArgs e)
+        {
+            if (serialPort.IsOpen)
+            {
+                serialPort.WriteLine("G28");
+            }
+        }
+    }
+}
